cc34be85f430af4d7a8a5ec282189871af49baef
Zajano <zackismet@gmail.com>
2020-05-13 19:52:06 -0700

    driver and scraping update

--- scrape_bgg.py
+++ scrape_bgg.py
@@ -53,16 +53,6 @@ for i in range(1,2):
 
 print(len(game_links))
 
-time.sleep(1)
-html = requests.get(game_links[0]).content
-
-# selector object to navigate
-sel = Selector(text = html)
-
-name_path = '//meta[@name="title"]/@content'
-
-print(sel.xpath(name_path).extract())
-
 game_info = []
 
 # for link in game_links:
@@ -75,37 +65,62 @@ for i in range(3):
     #get info from stats
     time.sleep(1)
     driver.get(stats)
-    title = driver.find_elements_by_xpath('//div[@class="game-header-title-info"]/h1/a')[0].text
-    avg_rating = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
-    /div[2]/div/div[2]/ul/li[1]/div[2]/a')[0].text
-    no_ratings = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
-    /div[2]/div/div[2]/ul/li[2]/div[2]/a')[0].text
-    complexity = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
-    /div[2]/div/div[2]/ul/li[4]/div[2]/a/span')[0].text
-    comments = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
-    /div[2]/div/div[2]/ul/li[5]/div[2]/a')[0].text
-    fans = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
-    /div[2]/div/div[2]/ul/li[6]/div[2]/a')[0].text
-    views = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
-    /div[2]/div/div[2]/ul/li[7]/div[2]')[0].text
+    title = driver.find_elements_by_xpath\
+        ('//div[@class="game-header-title-info"]/h1/a')[0].text
+    year = int(driver.find_elements_by_xpath\
+        ('//div[@class="game-header-title-info"]/h1/span')[0].text[1:-1])
+    min_players = int(driver.find_elements_by_xpath\
+        ('//ul[@class="gameplay"]/li[1]/div/span/span[1]')[0].text)
+    max_players = int(driver.find_elements_by_xpath\
+        ('//ul[@class="gameplay"]/li[1]/div/span/span[2]')[0].text[1:])
+    avg_time = int(driver.find_elements_by_xpath\
+        ('//ul[@class="gameplay"]/li[2]/div/span/span/span[1]')[0].text)
+    max_time = driver.find_elements_by_xpath\
+        ('//ul[@class="gameplay"]/li[2]/div/span/span/span[2]')
+    geek_age = driver.find_elements_by_xpath\
+        ('//ul[@class="gameplay"]/li[3]/div/span/')
 
-    addition = ((title,
-                 avg_rating,
-                 no_ratings,
-                 complexity,
-                 comments,
-                 fans,
-                 views))
-    game_info.append(addition)
+    #get average time
+    if max_time != []:
+        max_time = int(max_time[0].text[1:])
+        avg_time = int((avg_time + max_time) // 2)
 
-# print(game_info)
-df = pd.DataFrame(game_info,
-                  columns=['title',
-                           'avg_rating',
-                           'no_ratings',
-                           'complexity',
-                           'comments',
-                           'fans',
-                           'views'])
-df.to_csv('game_info.csv')
+    #check for age, chope off '+'
+    if geek_age != []:
+        geek_age = int(geek_age[0].text[:-1])
+
+    print(title, " ",year, " ", min_players, " ", max_players, " ", avg_time)
+
+    # avg_rating = driver.find_elements_by_xpath\
+    # ('//div[@class="row game-stats"]/div[2]/div/div[2]/ul/li[1]/div[2]/a')[0].text
+    # no_ratings = driver.find_elements_by_xpath\
+    # ('//div[@class="row game-stats"]/div[2]/div/div[2]/ul/li[2]/div[2]/a')[0].text
+    # complexity = driver.find_elements_by_xpath\
+    # ('//div[@class="row game-stats"]/div[2]/div/div[2]/ul/li[4]/div[2]/a/span')[0].text
+    # comments = driver.find_elements_by_xpath\
+    # ('//div[@class="row game-stats"]/div[2]/div/div[2]/ul/li[5]/div[2]/a')[0].text
+    # fans = driver.find_elements_by_xpath\
+    # ('//div[@class="row game-stats"]/div[2]/div/div[2]/ul/li[6]/div[2]/a')[0].text
+    # views = driver.find_elements_by_xpath\
+    # ('//div[@class="row game-stats"]/div[2]/div/div[2]/ul/li[7]/div[2]')[0].text
+
+#     addition = ((title,
+#                  avg_rating,
+#                  no_ratings,
+#                  complexity,
+#                  comments,
+#                  fans,
+#                  views))
+#     game_info.append(addition)
+#
+# # print(game_info)
+# df = pd.DataFrame(game_info,
+#                   columns=['title',
+#                            'avg_rating',
+#                            'no_ratings',
+#                            'complexity',
+#                            'comments',
+#                            'fans',
+#                            'views'])
+# df.to_csv('game_info.csv')
 driver.close()
