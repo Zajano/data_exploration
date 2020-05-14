871bf8fd90712114fb4daa1df98d38770ae9782e
Zajano <zackismet@gmail.com>
2020-05-13 17:20:16 -0700

    first scrape attempt

--- scrape_bgg.py
+++ scrape_bgg.py
@@ -5,9 +5,18 @@
 # scraping modules
 import requests
 from scrapy import Selector
+from selenium.webdriver import Chrome
+
+# pandas for csv
+import pandas as pd
+import csv
 
 # time delay for requests
 import time
+
+# driver to crawl links
+webdriver = "C:\\Users\\Zack\\Desktop\\OSU\\406 - p1 - stats\\test_files\\chromedriver.exe"
+driver = Chrome(webdriver)
 
 # starting urls for game list without page number
 url1 = 'https://boardgamegeek.com/browse/boardgame/page/'
@@ -54,6 +63,49 @@ name_path = '//meta[@name="title"]/@content'
 
 print(sel.xpath(name_path).extract())
 
-# html_file = open("html_stuff.txt", "w")
-# html_file.write(print(html))
-# html_file.close()
+game_info = []
+
+# for link in game_links:
+for i in range(3):
+
+    #pages with all the info I want
+    stats = game_links[i] + "/stats"
+    credits = game_links[i] + "/credits"
+
+    #get info from stats
+    time.sleep(1)
+    driver.get(stats)
+
+    title = driver.find_elements_by_xpath('//meta[@name="title"]/@content').extract
+    avg_rating = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
+    /div[2]/div/div[2]/ul/li[1]/div[2]/a/text()').extract
+    no_ratings = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
+    /div[2]/div/div[2]/ul/li[2]/div[2]/a/text()').extract
+    complexity = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
+    /div[2]/div/div[2]/ul/li[4]/div[2]/a/span/text()').extract
+    comments = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
+    /div[2]/div/div[2]/ul/li[5]/div[2]/a/text()').extract
+    fans = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
+    /div[2]/div/div[2]/ul/li[6]/div[2]/a/text()').extract
+    views = driver.find_elements_by_xpath('//div[@class="row game-stats"]\
+    /div[2]/div/div[2]/ul/li[7]/div[2]/text()').extract
+
+    addition = ((title,
+                 avg_rating,
+                 no_ratings,
+                 complexity,
+                 comments,
+                 fans,
+                 views))
+    game_info.append(addition)
+
+df = pd.DataFrame(game_info,
+                  columns=['title',
+                           'avg_rating',
+                           'no_ratings',
+                           'complexity',
+                           'comments',
+                           'fans',
+                           'views'])
+df.to_csv('game_info.csv')
+driver.close()
