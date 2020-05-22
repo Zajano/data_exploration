1e57bb250198bc438565bd45cf9e3eb0e17628a3
zajano <zackismet@gmail.com>
2020-05-21 17:28:28 -0700

    leetcode testfile

--- testfile.py
+++ testfile.py
@@ -1,1 +1,19 @@
-let's see if this updates'
+def maxArea(height):
+
+    largest = 0
+    start = 0
+
+    for h in height:
+        start += 1
+
+        for i in range(start + 1, len(height)):
+            dist = i - start
+            short = min(height[i], h)
+            container = dist * short
+            if container > largest:
+                largest = container
+
+    return largest
+
+eights = [1,8,6,2,5,4,8,3,7]
+print(maxArea(eights))
